1c37c626ca8256b9b53129806b03b3d80bacdbe4
Abdul734734 <s2500150.edu.bc.fi>
2025-06-04 14:54:52 +0200

    header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-   
-  
+      <div className='page-container'>
+        <Header name="abdul" />
+
+        <main>
+          <p>This is the main content of the app.</p>
+
+        </main>
+
+        <Footer year="2025" />
+      </div>
+
     </>
   )
 }
