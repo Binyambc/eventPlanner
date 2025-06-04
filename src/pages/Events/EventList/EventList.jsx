def46a0f2b8f1ca506b50a2fb462bc9fab6983cf
Hoa28686 <195274420+Hoa28686@users.noreply.github.com>
2025-06-04 03:05:52 +0300

    update EventCard, EventList and their module.css, add functions for save, delete, edit form, convert time date to object

--- src/pages/Events/EventList/EventList.jsx
+++ src/pages/Events/EventList/EventList.jsx
@@ -1,13 +1,35 @@
-import EventCard from "../../../components/eventCard/EventCard";
-import "./EventList.css";
+import EventCard from "../../../components/Event/EventCard/EventCard";
+import styles from "./EventList.module.css";
+const EventList = ({
+  eventData,
+  handleInfoChange,
+  error,
+  loading,
+  deleteEvent,
+  deleteError,
+}) => {
+  if (loading) {
+    return (
+      <div className={styles.loading}>
+        <h1>Loading......</h1>
+        <h2>It might take a little while</h2>
+      </div>
+    );
+  }
+  if (error) return <h1 className={styles.error}>Error: {error.message}</h1>;
 
-const EventList = ({ eventData }) => {
   return (
     <>
       <h1>EventList</h1>
       <div>
         {eventData.map((event) => (
-          <EventCard key={event.id} {...event} />
+          <EventCard
+            key={event.id}
+            {...event}
+            handleInfoChange={handleInfoChange}
+            deleteEvent={deleteEvent}
+            deleteError={deleteError}
+          />
         ))}
       </div>
     </>
